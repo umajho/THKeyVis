8fdc5b91d2cda8ff4215f798a18c9f1468b5b53c
umajho <umajho@proton.me>
2025-09-13 20:57:16 +0800

    Initial Commit (New Xcode macOS Project)

---
+++ THKeyVis/THKeyVisApp.swift
@@ -0,0 +1,17 @@
+//
+//  THKeyVisApp.swift
+//  THKeyVis
+//
+//  Created by Umaĵo on 2025/9/13.
+//
+
+import SwiftUI
+
+@main
+struct THKeyVisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ THKeyVisTests/THKeyVisTests.swift
@@ -0,0 +1,17 @@
+//
+//  THKeyVisTests.swift
+//  THKeyVisTests
+//
+//  Created by Umaĵo on 2025/9/13.
+//
+
+import Testing
+@testable import THKeyVis
+
+struct THKeyVisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
